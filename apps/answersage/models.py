f13744fec87f9238fb966f5c38b9dbaaec9d2945
nurbol0tt <fairtail764@gmail.com>
2023-08-11 01:24:31 +0600

    add tables

--- apps/answersage/models.py
+++ apps/answersage/models.py
@@ -1,3 +1,26 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+from apps.user.models import User, Consultant
+
+
+class Question(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    consultant = models.ForeignKey(Consultant, on_delete=models.CASCADE)
+    TOPIC_CHOICES = (
+        (1, 'Topic 1'),
+        (2, 'Topic 2'),
+    )
+    topic = models.IntegerField(choices=TOPIC_CHOICES)
+    text = models.TextField()
+    result = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+
+class Answer(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    question = models.OneToOneField(Question, on_delete=models.CASCADE)
+    answer = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
---
+++ apps/user/models.py
@@ -0,0 +1,21 @@
+import uuid
+
+from django.db import models
+
+
+class User(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    username = models.CharField(max_length=255)
+    phone = models.CharField(max_length=25)
+    photo = models.ImageField()
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.username
+
+
+class Consultant(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    telegram_id = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
