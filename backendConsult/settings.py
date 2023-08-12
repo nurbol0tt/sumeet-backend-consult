4810a7c7a32f71cdad00e408a1cf61faafd35f8b
nurbol0tt <fairtail764@gmail.com>
2023-08-12 13:56:33 +0600

    install rest

--- backendConsult/settings.py
+++ backendConsult/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+from os.path import join
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    "rest_framework",
 
     "apps.user",
     "apps.answersage"
@@ -119,7 +121,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+
+STATIC_URL = "/static/"
+STATIC_ROOT = (
+    join(BASE_DIR.parent, 'static')
+)
+
+MEDIA_URL = '/media/'
+MEDIA_DIR = join('BASE_DIR', 'media')
+MEDIA_ROOT = MEDIA_DIR
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
